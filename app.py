cc5ccab751b16c6a5b7384916569eaba958fb4c4
Oshan Goyanka <oshangoyanka@gmail.com>
2026-06-02 22:37:39 +0530

    Updated hamper bot flow

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import requests
 import os
 
-app = Flask(**name**)
+app = Flask(__name__)
 
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
