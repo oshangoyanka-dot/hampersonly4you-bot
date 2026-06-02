ce63ef2ac3a465f9d5642ee87704f23d3745849a
Oshan Goyanka <oshangoyanka@gmail.com>
2026-06-02 18:20:46 +0530

    Render port fix

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "HampersOnly4You Bot is Running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
